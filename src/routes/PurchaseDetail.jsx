5ff0faf0ecd60685a7f2c9ff34bf011ebc719900
Jong jae Park <pjj186@naver.com>
2022-04-30 12:45:56 +0900

    게시글 상세보기 페이지

--- src/routes/PurchaseDetail.jsx
+++ src/routes/PurchaseDetail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 import BoardTitle from "../components/BoardTitle";
 import { useLocation } from "react-router-dom";
+import LongMenu from "../components/ui/LongMenu";
 
 const PurchaseDetail = () => {
   const location = useLocation();
@@ -13,17 +14,62 @@ const PurchaseDetail = () => {
       </Helmet>
       <div className="w-full h-full">
         <BoardTitle title="구매 문의 게시판" />
-        <div className="w-full h-full bg-orange-300">
-          {/* 게시글 번호, 제목 */}
-          <div className="w-full h-full flex justify-center items-center ">
-            <div className="flex items-center justify-between bg-slate-400 w-[60%] h-8 px-10 font-semibold">
-              <div className="">1</div>
-              <div className="">얼마죠?</div>
-              <div className="">2022-04-30</div>
+        <div className="w-full h-full flex justify-center">
+          <div className="w-[50%] mb-20 bg-white border border-black shadow-md px-5">
+            {/* 게시글 머리 */}
+            <div className="py-4">
+              <div className="text-[#02C75A] text-sm mb-1">구매 문의</div>
+              <div className="text-3xl font-semibold">제목</div>
+              <div className="flex items-center py-2 justify-between">
+                <div className="flex items-center">
+                  <div className="bg-slate-400 w-10 h-10 rounded-full mr-3" />
+                  <div className="flex flex-col">
+                    <span className="font-semibold text-base">박종재</span>
+                    <span className="text-sm text-gray-400">2022-04-30</span>
+                  </div>
+                </div>
+                <div className=" text-base text-gray-500">
+                  <span>수정</span>
+                  <span className="ml-2">삭제</span>
+                </div>
+              </div>
+            </div>
+            <hr className="mb-4" />
+            {/* 게시글 몸 */}
+            <div className="h-[600px]">게시글게시글</div>
+            <hr className="mb-3" />
+            <div>
+              <div className="font-bold text-lg mb-4">댓글</div>
+              {/* 댓글 내용 */}
+              <div className="flex w-full justify-between">
+                <div>
+                  <div className="font-semibold text-base">공돌이</div>
+                  <div className="text-sm">라즈베리파이 4 정석</div>
+                  <div className="text-xs mb-2 text-gray-500">2022-04-30</div>
+                </div>
+                <LongMenu />
+              </div>
+              <hr className="mb-2" />
+              <div className="flex w-full justify-between">
+                <div>
+                  <div className="font-semibold text-base">공돌이</div>
+                  <div className="text-sm">라즈베리파이 4 정석</div>
+                  <div className="text-xs mb-2 text-gray-500">2022-04-30</div>
+                </div>
+                <LongMenu />
+              </div>
+              <hr className="mb-2" />
+              <div className="flex w-full justify-between">
+                <div>
+                  <div className="font-semibold text-base">공돌이</div>
+                  <div className="text-sm">라즈베리파이 4 정석</div>
+                  <div className="text-xs mb-2 text-gray-500">2022-04-30</div>
+                </div>
+                <LongMenu />
+              </div>
+              <hr className="mb-2" />
             </div>
           </div>
-          {/* 게시글 내용 */}
-          <div>나도 몰러</div>
         </div>
       </div>
     </>
