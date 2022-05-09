cdd80e902bdaa04a50c0c642002984f15f0f4284
Jong jae Park <pjj186@naver.com>
2022-05-10 02:04:27 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,31 +45,28 @@ const Header = () => {
           <Link to="/report">고장 신고</Link>
         </div>
         <div className=" flex items-center justify-evenly w-[310px]">
-          {userCookie.user_id !== "" ? (
-            <>
-              {loginContext.setIsLogin(true)}
-              <Link to="/profile">
-                <div className="flex items-center">
-                  <div className="rounded-full w-10 h-10 mr-2 flex items-center justify-center border border-green-200 bg-[#90C8B4]">
-                    <svg
-                      className="w-6 h-6 text-white"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                      />
-                    </svg>
-                  </div>
-                  <span>{userCookie.user_name}</span>
+          {loginContext.isLogin ? (
+            <Link to="/profile">
+              <div className="flex items-center">
+                <div className="rounded-full w-10 h-10 mr-2 flex items-center justify-center border border-green-200 bg-[#90C8B4]">
+                  <svg
+                    className="w-6 h-6 text-white"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                    />
+                  </svg>
                 </div>
-              </Link>
-            </>
+                <span>{userCookie.user_name}</span>
+              </div>
+            </Link>
           ) : (
             <Link to="/login">
               <button className="bg-[#2F74F1] text-white w-[100px] h-[35px] rounded-md">
@@ -77,7 +74,7 @@ const Header = () => {
               </button>
             </Link>
           )}
-          {userCookie.user_id !== "" ? (
+          {loginContext.isLogin ? (
             <div>
               <button
                 className="cursor-pointer hover:underline"
