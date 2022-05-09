446d6bd2e58806c808caa978db55589bc38f0878
Jong jae Park <pjj186@naver.com>
2022-05-09 21:00:31 +0900

    종재 - Login Test

--- src/HOC/AuthHOC.jsx
+++ src/HOC/AuthHOC.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App";
 import { getCookie } from "../components/auth/cookie";
 
@@ -6,11 +7,10 @@ const AuthHOC = (SpecificComponent, option, adminRoute = null) => {
   // Component : 컴포넌트
   // option : null (누구든 출입 가능) true(로그인 한 유저만 출입가능) false (로그인 한 유저는 출입 불가)
 
-  const LoginContext = useContext(AppContext);
-
   const AuthenticationCheck = (props) => {
+    const LoginContext = useContext(AppContext);
     // 유저 인증 처리
-    const handleAuth = useCallback(() => {
+    const handleAuth = () => {
       if (getCookie("x_auth").user_id != null) {
         LoginContext.setUser({
           email: getCookie("x_auth").user_id,
@@ -19,7 +19,7 @@ const AuthHOC = (SpecificComponent, option, adminRoute = null) => {
       } else {
         console.log("no login");
       }
-    });
+    };
 
     useEffect(() => {
       handleAuth();
