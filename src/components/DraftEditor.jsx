f2166adc9fac0d197ed1ae089ae2adb0a2b504f5
Jong jae Park <pjj186@naver.com>
2022-05-11 15:50:40 +0900

    Update DraftEditor.jsx

--- src/components/DraftEditor.jsx
+++ src/components/DraftEditor.jsx
@@ -29,6 +29,8 @@ const MyBlock = styled.div`
 // title props는 게시글 제목, mode props는 게시글 수정 모드
 const DraftEditor = ({ title, mode, articleId, editContent, board }) => {
   let socialUser = sessionStorage.getItem("socialUser");
+
+  console.log(socialUser.email);
   const userCookie = getCookie("x_auth");
   const navigate = useNavigate();
   // useState로 상태관리하기 초기값은 EditorState.createEmpty()
