4f7d85c0e56dca5ce16c222a0eeeac918a2e6f91
Jong jae Park <pjj186@naver.com>
2022-05-12 08:56:36 +0900

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import * as Scroll from "react-scroll";
 import { getCookie } from "./auth/cookie";
 import { useCookies } from "react-cookie";
-import { identifyUserName } from "../utils/utils";
+import { identifyUserName, identifyUserId } from "../utils/utils";
 
 const Header = () => {
   const location = useLocation();
@@ -70,7 +70,9 @@ const Header = () => {
                   </svg>
                 </div>
                 <span className="font-semibold">
-                  {identifyUserName(userCookie, socialUser)}
+                  {userCookie || socialUser
+                    ? identifyUserName(userCookie, socialUser)
+                    : null}
                 </span>
               </div>
             </Link>
