87f60f91b10c219e5aabe9760a87c9657faf2f27
Jong jae Park <pjj186@naver.com>
2022-05-11 16:31:48 +0900

    Update AuthHOC.jsx

--- src/HOC/AuthHOC.jsx
+++ src/HOC/AuthHOC.jsx
@@ -6,8 +6,6 @@ const AuthHOC = (SpecificComponent, mode = null, option, adminRoute = null) => {
   // Component : 컴포넌트
   // option : null (누구든 출입 가능) true(로그인 한 유저만 출입가능) false (로그인 한 유저는 출입 불가)
   const AuthenticationCheck = (props) => {
-    console.log(mode);
-
     // 유저 인증 처리
     const LoginContext = useContext(AppContext);
     let userCookie = getCookie("x_auth");
@@ -24,6 +22,8 @@ const AuthHOC = (SpecificComponent, mode = null, option, adminRoute = null) => {
         LoginContext.setIsLogin(true);
       }
     }, []);
+
+    console.log(mode);
 
     if (mode === "edit") {
       console.log(mode);
