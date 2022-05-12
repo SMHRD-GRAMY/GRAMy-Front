267aa87d8594cd3b40263d14e8445da5173938dc
Jong jae Park <pjj186@naver.com>
2022-05-12 19:30:41 +0900

    [종재] 헤더 버튼 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -84,7 +84,7 @@ const Header = () => {
             </Link>
           ) : (
             <Link to="/login">
-              <button className="bg-[#2F74F1] text-white w-[100px] py-3 rounded-md font-semibold">
+              <button className="bg-[#2F74F1] text-white w-[100px] py-3 text-base rounded-md font-semibold">
                 로그인
               </button>
             </Link>
@@ -92,7 +92,7 @@ const Header = () => {
           {loginContext.isLogin ? (
             <div>
               <button
-                className="bg-[#2F74F1] text-white w-[100px] py-3 rounded-md font-semibold"
+                className="bg-[#2F74F1] text-white w-[100px] py-3 rounded-md text-base font-semibold"
                 onClick={handleLogout}
               >
                 로그아웃
@@ -100,7 +100,7 @@ const Header = () => {
             </div>
           ) : (
             <Link to="/join">
-              <button className="bg-[#2F74F1] text-white w-[100px] py-3 rounded-md font-semibold">
+              <button className="bg-[#2F74F1] text-white w-[100px] py-3 rounded-md text-base font-semibold">
                 회원가입
               </button>
             </Link>
