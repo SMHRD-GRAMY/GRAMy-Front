bbb491a0317ba6be7f66106df67915e990f50d44
Jong jae Park <pjj186@naver.com>
2022-05-12 12:48:57 +0900

    Update Profile.jsx

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,9 +1,13 @@
 import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
 import { AppContext } from "../App";
 
 const Profile = () => {
   const loginContext = useContext(AppContext);
   const { isLogin } = loginContext;
+  const params = useParams();
+
+  console.log(params);
   return <div>유저 프로필</div>;
 };
 
