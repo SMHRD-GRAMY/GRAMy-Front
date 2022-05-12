b5ce3e35df03c7b18da8c4bd4ff483fec839b889
Jong jae Park <pjj186@naver.com>
2022-05-12 16:36:53 +0900

    [종재] 회원 수정 기능 구현중

--- src/routes/EditProfile.jsx
+++ src/routes/EditProfile.jsx
@@ -44,6 +44,16 @@ const EditProfile = () => {
       .then((res) => {
         console.log(res.data);
         setUserData(res.data);
+      })
+      .then(() => {
+        setInput({
+          user_id: userData.user_id,
+          user_pw: "",
+          user_name: userData.user_name,
+          user_phone: "",
+          user_gender: userData.user_gender,
+          user_addr: userData.user_addr,
+        });
         setLoading(false);
       });
   };
@@ -72,14 +82,6 @@ const EditProfile = () => {
 
   useEffect(() => {
     loadUserInfo();
-    setInput({
-      user_id: userData.user_id,
-      user_pw: "",
-      user_name: userData.user_name,
-      user_phone: "",
-      user_gender: userData.user_gender,
-      user_addr: userData.user_addr,
-    });
   }, []);
   return (
     <>
