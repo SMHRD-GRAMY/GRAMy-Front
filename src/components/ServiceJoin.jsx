984670421f7a2ceb4ffec21982ebd200e26d362b
Jong jae Park <pjj186@naver.com>
2022-05-18 09:05:23 +0900

    [종재] ServiceJoin 버튼 border 좀 더 얇게

--- src/components/ServiceJoin.jsx
+++ src/components/ServiceJoin.jsx
@@ -10,7 +10,7 @@ const ServiceJoin = () => {
 
   return (
     <div
-      className=" mt-20 cursor-pointer bg-white inline-flex items-center justify-center px-5 py-3 border-transparent text-base hover:bg-[#132C4D] hover:text-white leading-6 rounded-md text-gray-800 focus:outline-none focus:ring border-2 border-gray-700 transition duration-150 ease-in-out md:text-lg md:px-6 z-10"
+      className=" mt-20 cursor-pointer bg-white inline-flex items-center justify-center px-5 py-3 border-transparent text-base hover:bg-[#132C4D] hover:text-white leading-6 rounded-md text-gray-800 focus:outline-none focus:ring border border-gray-700 transition duration-150 ease-in-out md:text-lg md:px-6 z-10"
       onClick={onClickServiceJoin}
     >
       <span className=" font-bold mr-2">서비스 신청</span>
